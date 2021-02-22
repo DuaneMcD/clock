86086832a8cb1f9daa089cf5d540b905ee34772c
Duane McDonald <mcdonald.duane13@gmail.com>
2021-02-22 09:52:54 -0500

    logic clean & current
    all var updated
    variables renamed for readability
    rerouted logic more cleanly

--- clock.js
+++ clock.js
@@ -1,14 +1,20 @@
 //clock with hours:minutes:seconds
 // hours converted to 12hr type
 // setInterval is essentially a refresh every 1sec
-setInterval(timer, 1000)
-function timer(){
-    var clock = new Date();
-    var hh = String((clock.getHours() + 24) % 12 || 12).padStart(2, "0");
-    var mm = String(clock.getMinutes()).padStart(2, "0");
-    var ss = String(clock.getSeconds()).padStart(2, "0");
-    var meridiem = String(clock.getHours()) > 12 ? "PM" : "AM";
+const clockDisplay = () => {
+    let date = new Date();
+    const hours = addLeadingZero(((date.getHours() + 24) % 12 || 12));
+    const minutes = addLeadingZero(date.getMinutes());
+    const seconds = addLeadingZero(date.getSeconds());
+    const meridiem = date.getHours() > 12 ? "PM" : "AM";
     
-    clock = hh + ":" + mm + ":" + ss + " " + meridiem;
-    document.getElementById("clock").innerHTML = clock;
-}
+    display = hours + ":" + minutes + ":" + seconds + " " + meridiem;
+    document.getElementById("clock").textContent = display;
+};
+
+const addLeadingZero = (number) => {
+    return number < 10 ? '0' + number : number;
+};
+
+clockDisplay();
+setInterval(clockDisplay, 1000)
