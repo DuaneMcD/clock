4248b54815864e85da96938e7fb1b6b814b3d17d
Duane McDonald <mcdonald.duane13@gmail.com>
2021-02-22 10:56:11 -0500

    Update date.js
    cleaned up logic
    var to const
    innerHTML -> textConent (more secure)
    adjusted assignments for readability

--- date.js
+++ date.js
@@ -1,21 +1,32 @@
-var today = new Date();
-        var days = ["Sunday", "Monday", "Tueday", "Wedday", "Thuday", "Friday", "Satday"];
-        var dayName = days[today.getDay()];
-        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        var monthName = months[today.getMonth()];
-        var dd = String(today.getDate()).padStart(2, "0");
-        const toOrdinalSuffix = num => {
-                const int = parseInt(num),
-                  digits = [int % 10, int % 100],
-                  ordinals = ['st', 'nd', 'rd', 'th'],
-                  oPattern = [1, 2, 3, 4],
-                  tPattern = [11, 12, 13, 14, 15, 16, 17, 18, 19];
-                return oPattern.includes(digits[0]) && !tPattern.includes(digits[1])
-                  ? int + ordinals[digits[0] - 1]
-                  : int + ordinals[3];
-              };
-        var ordinalSuffix = toOrdinalSuffix(dd)
-        var yyyy = today.getFullYear();
+//establish updated variables; array of names
+//accounts for location/timezone etc
+const today = new Date();
+const date = today.getDate();
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-        today = dayName + ", " + monthName + " " + ordinalSuffix + " " + yyyy;
-        document.getElementById("date").innerHTML = today;
+//function to build current date display
+const displayDate = () => {
+  const dayName       = days[today.getDay()] + ", ";
+  const monthName     = months[today.getMonth()]+ " ";
+  const ordinalSuffix = toOrdinalSuffix(date)+ " ";
+  const year          = today.getFullYear();
+  
+  const display = dayName + monthName + ordinalSuffix + year;
+  document.getElementById("date").textContent = display;
+}
+
+const toOrdinalSuffix = (number) => {
+  if (number < 10 || number > 20) {
+    switch (number % 10) {
+      case 1:
+        return number + 'st';
+      case 2:
+        return number + 'nd';
+      case 3:
+        return number + 'rd';
+    }
+    return number + 'th';
+  }
+};
+displayDate();
